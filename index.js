dabf235f8bab7ff9b1ac0ee38601a94fc5141567
vini00784 <vo70346@gmail.com>
2023-02-08 11:18:34 -0300

    get product by pk (or ID)

--- index.js
+++ index.js
@@ -4,14 +4,14 @@
     const Product = require('./product.js')
     await database.sync()
 
-    const newProduct = await Product.create({
-        nome: 'Teclado USB',
-        preco: 30,
-        descricao: 'Teclado bacana'
-    })
-    console.log(newProduct)
+    // const newProduct = await Product.create({
+    //     nome: 'Teclado USB',
+    //     preco: 30,
+    //     descricao: 'Teclado bacana'
+    // })
+    // console.log(newProduct)
 
-    const allProducts = await Product.findByPk()
+    const allProducts = await Product.findByPk(2)
     console.log(allProducts)
 
 })()
